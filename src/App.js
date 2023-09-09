33f7493d81c3801155162a6a24cb282aae6c9a62
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-09-10 01:24:38 +0300

    Make adding toDoItem

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import "./styles/App.css"
-import react, { useState } from "react"
-import { Input, Button, Space } from "antd"
-import { UserOutlined, QuestionOutlined } from "@ant-design/icons"
+import React from "react"
+import { useState } from "react"
+import {
+	Input,
+	Button,
+	Avatar,
+	Space,
+	Divider,
+	List,
+	Checkbox,
+	message,
+} from "antd"
+import { UserOutlined } from "@ant-design/icons"
 
 function App() {
-	const [toDoInput, setToDoInput] = useState({ status: true, text: "" })
+	const [toDoInput, setToDoInput] = useState({
+		status: true,
+		text: "",
+		id: null,
+	})
+	const [toDoItems, setToDoItems] = useState([
+		"Сходить за покупками",
+		"Убрать за котом",
+		"Порешать задчи с CodeWars",
+		"Подготовить сдачу лабораторной",
+	])
 
 	const pushToDo = (e) => {
 		e.preventDefault()
 		setToDoInput({ ...toDoInput, text: "" })
-		console.log(toDoInput.text)
+		setToDoItems([...toDoItems, toDoInput.text])
+		messageApi.open({
+			type: "success",
+			content: "Добавлено в список",
+			duration: 1,
+		})
 	}
+
+	const deleteToDo = (e) => {
+		e.preventDefault()
+	}
+	const [messageApi, contextHolder] = message.useMessage()
 
 	return (
 		<div className="App">
@@ -18,31 +48,48 @@ function App() {
 				style={{
 					display: "flex",
 					justifyContent: "flex-end",
+					alignItems: "center",
 					paddingBottom: "25px",
 				}}
 			>
-				<Button type="primary" ghost>
+				<Button size="small" type="primary" ghost>
 					Сменить тему
 				</Button>
 			</nav>
+
 			<Space.Compact>
 				<Input
 					placeholder="что нужно сделать"
 					value={toDoInput.text}
 					onChange={(e) => setToDoInput({ ...toDoInput, text: e.target.value })}
 					onKeyPress={(e) => (e.key === "Enter" ? pushToDo(e) : "")}
-					size="large"
+					size="small"
 					style={{ minWidth: "400px" }}
 				/>
+				{contextHolder}
 				<Button
 					type="primary"
 					disabled={toDoInput.text === "" ? true : ""}
 					onClick={pushToDo}
-					size="large"
+					size="medium"
 				>
 					отправить
 				</Button>
 			</Space.Compact>
+			<Divider orientation="center">To Do</Divider>
+			<List
+				size="medium"
+				bordered
+				dataSource={toDoItems}
+				renderItem={(item) => (
+					<List.Item style={{ fontSize: "15px" }}>
+						<Space wrap size={10}>
+							<Checkbox></Checkbox>
+							{item}
+						</Space>
+					</List.Item>
+				)}
+			/>
 		</div>
 	)
 }
